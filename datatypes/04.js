641f5f7430c5f05b0d9c07346f709f491c9b0912
unknown <ankit4gaurav@gmail.com>
2024-01-07 16:29:29 +0530

    first commit

---
+++ datatypes/04.js
@@ -0,0 +1,19 @@
+// Variables
+// let, CONST AND VAR  
+
+const userEmail = "gaurav@cogdigital.in";
+let userName = "Ankit Kumar";
+var userPassword = "32323333333";
+userPhone = 9654195768;
+
+//  Changing the values
+userName = "Gaurav Kumar";
+userPassword = "gaurav123";
+userPhone = 9654195767;
+
+
+/*
+Prefer not to use var to declared the variable
+Because it occurs issue in block scope and functional scope
+*/
+console.table([userEmail, userName, userPassword, userPhone]);
---
+++ datatypes/05.js
@@ -0,0 +1,19 @@
+"use strict";  //Treat all JS code as newer version
+
+//alert('hello'); //This will not work because we run this code in nodejs engine not in browser
+
+
+// Primittive data types
+// number => 12
+// string => ""
+// boolean => true/false
+// null => standalone value, it is kind of value 
+// undefined => variable is defined but not assigned value
+// symbol => unique
+
+// Non-primittive data types
+// object 
+// array
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
